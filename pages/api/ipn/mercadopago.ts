3a207e6fdf290a9f4af198a37cf3879189b26be3
Giuliana Erro <giulierro7@gmail.com>
2022-10-29 17:07:42 -0300

    delete comments mercadopago

--- pages/api/ipn/mercadopago.ts
+++ pages/api/ipn/mercadopago.ts
@@ -6,17 +6,7 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
   const { topic, id } = req.query;
 
   if (topic == "merchant_order") {
-    const order = await merchantOrder(id);
-    // console.log(order);
-    // if (order.order_status == "paid") {
-    //   const orderId = order.external_reference;
-    //   const myOrder = new Order(orderId);
-    //   myOrder.pull();
-    //   myOrder.data.status = "closed";
-    //   await myOrder.push();
-    //send email to user with order details (tu pago fue confirmado)
-    //send email to admin with order details (alguien compro algo)
-    // }
+   await merchantOrder(id);
   }
   res.send("ok");
 }
